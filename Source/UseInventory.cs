577e630151656a6edae97d3240a56aeff2a583ea
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2019-03-23 06:00:25 -0700

    Finding item in inventory

--- Source/UseInventory.cs
+++ Source/UseInventory.cs
@@ -23,29 +23,51 @@ namespace Build_From_Inventory
 				AccessTools.Method(typeof(UseInventory), nameof(OrUseInventory)));
 
 			return Harmony.Transpilers.MethodReplacer(instructions,
-				AccessTools.Method(typeof(GenClosest), nameof(ItemAvailability.ThingsAvailableAnywhere)),
+				AccessTools.Method(typeof(ItemAvailability), nameof(ItemAvailability.ThingsAvailableAnywhere)),
 				AccessTools.Method(typeof(UseInventory), nameof(OrInInventory)));
 		}
 
 		//public static Thing ClosestThingReachable(IntVec3 root, Map map, ThingRequest thingReq, PathEndMode peMode, TraverseParms traverseParams, float maxDistance = 9999f, Predicate<Thing> validator = null, IEnumerable<Thing> customGlobalSearchSet = null, int searchRegionsMin = 0, int searchRegionsMax = -1, bool forceGlobalSearch = false, RegionType traversableRegionTypes = RegionType.Set_Passable, bool ignoreEntirelyForbiddenRegions = false)
 		public static Thing OrUseInventory(IntVec3 root, Map map, ThingRequest thingReq, PathEndMode peMode, TraverseParms traverseParams, float maxDistance = 9999f, Predicate<Thing> validator = null, IEnumerable<Thing> customGlobalSearchSet = null, int searchRegionsMin = 0, int searchRegionsMax = -1, bool forceGlobalSearch = false, RegionType traversableRegionTypes = RegionType.Set_Passable, bool ignoreEntirelyForbiddenRegions = false)
 		{
-			Log.Message($"Looking for {thingReq.singleDef}");
 			Thing thing = GenClosest.ClosestThingReachable(root, map, thingReq, peMode, traverseParams, maxDistance, validator, customGlobalSearchSet, searchRegionsMin, searchRegionsMax, forceGlobalSearch, traversableRegionTypes, ignoreEntirelyForbiddenRegions);
 			if (thing != null) return thing;
 
-			ThingDef def = thingReq.singleDef;
-			Log.Message($"Didn't found {def}");
+			thing = FindInInventory(thingReq.singleDef, traverseParams.pawn);
+
+			Log.Message($"Found: {thingReq.singleDef}? ({thing})");
+		
+			return thing;
+		}
+
+		public static Thing FindInInventory(ThingDef def, Pawn worker)
+		{
+			//Worker first
+			foreach (Thing t in worker.inventory.GetDirectlyHeldThings())
+				if (t.def == def)
+					return t;
+
+			//Others next. A little redundant on worker but it'll be empty
+			foreach (Pawn p in worker.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer))
+			{
+				Log.Message($"Pawn: {p}");
+				foreach (Thing t in p.inventory.GetDirectlyHeldThings())
+				{
+					Log.Message($"-Thing: {t}");
+					if (t.def == def)
+						return t;
+				}
+			}
 			return null;
 		}
 
+		//TODO: Transpile in ThingsAvailableAnywhere to search inv, count need.count, and save to cache
 		//public bool ThingsAvailableAnywhere(ThingDefCountClass need, Pawn pawn)
-		public bool OrInInventory(ItemAvailability item, ThingDefCountClass need, Pawn pawn)
+		public static bool OrInInventory(ItemAvailability item, ThingDefCountClass need, Pawn pawn)
 		{
 			if (item.ThingsAvailableAnywhere(need, pawn)) return true;
 
 			ThingDef def = need.thingDef;
-			Log.Message($"No {def} on map");
 			return pawn.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer)
 				.Any(p => p.inventory?.GetDirectlyHeldThings().Any(t => t.def == def) ?? false);
 		}
