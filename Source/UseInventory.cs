78e7e52a25341ea85a47d88c464eab7d51a01419
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-02-12 02:09:10 -0800

    Check reservations of inventory goods because it might be hauling with pick up and haul

--- Source/UseInventory.cs
+++ Source/UseInventory.cs
@@ -51,7 +51,8 @@ namespace Build_From_Inventory
 			foreach (Pawn p in worker.Map.mapPawns.SpawnedPawnsInFaction(Faction.OfPlayer)
 				.Where(p =>!p.Position.IsForbidden(worker) &&	worker.CanReach(p, PathEndMode.OnCell, Danger.Some)))
 				foreach (Thing t in p.inventory.GetDirectlyHeldThings())
-					if (t.def == def)
+					//private WorkGiver_ConstructDeliverResources.ResourceValidator :(
+					if (t.def == def && worker.CanReserve(t))
 						return t;
 
 			return null;
