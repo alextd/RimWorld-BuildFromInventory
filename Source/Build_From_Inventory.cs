440e71d8b11c0a9279bfd334e89c124ade3da26c
Alex Tearse-Doyle <alextearsedoyle@gmail.com>
2020-08-30 13:21:51 -0700

    removing DefOf Warning suppression. Seems to not be a problem anymore.

--- Source/Build_From_Inventory.cs
+++ Source/Build_From_Inventory.cs
@@ -18,19 +18,7 @@ namespace Build_From_Inventory
 #endif
 
 			Harmony harmony = new Harmony("Uuugggg.rimworld.Build_From_Inventory.main");
-
-			//Turn off DefOf warning since harmony patches trigger it.
-			MethodInfo DefOfHelperInfo = AccessTools.Method(typeof(DefOfHelper), "EnsureInitializedInCtor");
-			if (!harmony.GetPatchedMethods().Contains(DefOfHelperInfo))
-				harmony.Patch(DefOfHelperInfo, new HarmonyMethod(typeof(Mod), "EnsureInitializedInCtorPrefix"), null);
-			
 			harmony.PatchAll();
-		}
-
-		public static bool EnsureInitializedInCtorPrefix()
-		{
-			//No need to display this warning.
-			return false;
 		}
 
 //		public override void DoSettingsWindowContents(Rect inRect)
